09494532548d03a9cac2ca5c51adeea0e0e8527e
Ellie <cassona08@gmail.com>
2019-11-08 19:56:31 +0000

    completed functions up to add_pet_to_stock

--- pet_shop.rb
+++ pet_shop.rb
@@ -92,7 +92,17 @@ def find_pet_by_name(pet_shop, pet_name)
   nil
 end
 
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+    pet_shop[:pets].delete(pet)
+    end
+  end
+end
 
+def add_pet_to_stock(pet_shop, new_pet)
+pet_shop[:pets].push(new_pet)
+end
 
 
 
